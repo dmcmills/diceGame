d3d3b7a97d0ba78655252735c67ebc89dbaecf1c
dmcmills <derick.l.mcm@gmail.com>
2017-12-07 20:24:06 -0600

    Added Files

---
+++ diceGame.js
@@ -0,0 +1,15 @@
+"use strict"
+
+var lengthOfField = (200);
+var player1= ("Jack");
+var player2= ("Jill");
+var die1= ("Die A"); //4 sided die
+var die2= ("Die B"); //6 sided die
+var die3= ("Die C"); //8 sided die
+var die4= ("Die D"); //10 sided die
+var die5= ("Die E"); //12 sided die
+var die6= ("Die F"); //20 sided die
+
+function dieA(Die A) {
+	if 
+}
